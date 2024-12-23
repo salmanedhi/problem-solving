e47254153526be88c0894fbf41eaaa752433100c
SYSTEMS\msalman.edhi <msalman.edhi@systemsltd.com>
2024-12-23 16:21:02 +0500

    Remove Duplicates

---
+++ src/main/java/com/problem/solving/service/Impl/RemoveDuplicates.java
@@ -0,0 +1,27 @@
+package com.problem.solving.service.Impl;
+
+import java.util.Arrays;
+
+
+/**
+ * Given a sorted list of numbers, remove duplicates and return the new length. You must do this in-place and without using extra memory.
+ * Input: [0, 0, 1, 1, 1, 2, 2].
+ * Output: 3.
+ * Your function should modify the list in place so that the first three elements become 0, 1, 2. Return 3 because the new length is 3.
+ */
+public class RemoveDuplicates  {
+
+  public static void main(String[] args) {
+   int[] arr = {0, 1,1,2,2,2};
+
+   int i=0, j=0;
+   while(j < arr.length) {
+     if(arr[i] != arr[j]) {
+       arr[++i] = arr[j];
+     }
+     j++;
+   }
+    System.out.println(Arrays.toString(arr));
+    System.out.println(i+1);
+  }
+}
